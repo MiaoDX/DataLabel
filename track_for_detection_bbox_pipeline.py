c5d846ca6fb18b9fefac8a36005d1a0bd6f72194
miaodx <miaodx@hotmail.com>
2018-03-17 10:32:38 +0800

    track for detection.

---
+++ track_for_detection_bbox_pipeline.py
@@ -0,0 +1,11 @@
+from conf.conf_loader import des_dir_conf
+from track_for_detection_bbox.track_by_detection import track_with_manual_label_all, show_all_the_bbox
+from track_for_detection_bbox.unified_tracking import get_one_tracker
+
+if __name__ == '__main__':
+
+    global_tracker = get_one_tracker('DLIB')
+
+    track_with_manual_label_all(des_dir_conf, now_split=0, label_name='armer', global_tracker_=global_tracker)
+
+    # show_all_the_bbox(des_dir_conf)
